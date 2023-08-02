171161bba10ab3a4a6b9f9f65148dda800bcafcd
Piergiorgio Gonni <pg@sekei.ca>
2023-08-02 17:38:32 -0400

    Initial Commit

---
+++ StageGuide/StageGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageGuideApp.swift
+//  StageGuide
+//
+//  Created by Piergiorgio Gonni on 2023-08-02.
+//
+
+import SwiftUI
+
+@main
+struct StageGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
